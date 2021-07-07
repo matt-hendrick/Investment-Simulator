993867ca6607d185f13464ba94591e4895adc8e6
Matt Hendrick <75456865+matt-hendrick@users.noreply.github.com>
2021-07-07 17:52:49 -0400

    Rename: simulate to calculate

---
+++ calculateStockChange.py
@@ -0,0 +1,19 @@
+import math
+import numpy as np
+
+
+def calculateStockChangeForAYear(price_series):
+
+    total_growth = price_series['Close'][-1] / price_series['Close'][0]
+
+    # determines years elapsed by calculating size of price_series DF and dividing by trading days
+    # there are 252 trading days in the year
+    years_elapsed = (len(price_series))/252
+    annualized_growth_rate = total_growth / years_elapsed
+
+    # gets standard deviation of closing price_series
+    std = price_series["Close"].pct_change().std()
+    std = std * math.sqrt(252)
+
+    return np.random.normal(
+        (annualized_growth_rate/252), std/math.sqrt(252), 21)+1
